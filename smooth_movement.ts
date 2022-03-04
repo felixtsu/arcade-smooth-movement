91d92476dc609e4144c3cf3eabaa0a5f9f36169f
felixtsu <humaeks@gmail.com>
2022-03-04 20:25:21 +0800

    更新 pxt.json, smooth_movement.ts

--- smooth_movement.ts
+++ smooth_movement.ts
@@ -3,10 +3,20 @@
 namespace smooth_movement {
 
     //% block
-    //% vx.defl = 50
-    //% vy.defl = 50
+    //% vx.defl=100 vy.defl=100
+    //% expandableArgumentMode="toggle"
     //% blockid=smooth_movement_move_sprite block="用控制器顺滑移动 %sprite=variables_get(mySprite) || vx $vx vy $vy"
-    export function moveSpriteSmoothly(sprite:Sprite, vx:number=50, vy : number=50) {
+    //% vx.shadow="spriteSpeedPicker"
+    //% vy.shadow="spriteSpeedPicker"
+    export function moveSpriteSmoothly(sprite:Sprite, vx:number=100, vy : number=100) {
         smoothturn.moveSprite(sprite, vx, vy)
+    }
+
+    //% block
+    //% speed.defl=50
+     //% blockid=smooth_movement_random_move_sprite block="以速度 %speed 随机移动 %sprite=variables_get(mySprite) "
+    export function randomMovement(sprite:Sprite,speed:number=50) {
+        sprite_movement.randomlyMoveSprite(sprite, speed)
+        
     }
 }
